cd47c9f3dfd9d622445c70c79b5ff6c4980d4d60
Alipour <za.alipour.java@gmail.com>
2021-04-27 22:18:38 +0430

    Add sort to list panel

--- frontend/src/components/user/UserListPanel.js
+++ frontend/src/components/user/UserListPanel.js
@@ -1,40 +1,55 @@
 import {
   Avatar,
   Box,
+  IconButton,
   List,
   ListItem,
   ListItemAvatar,
   ListItemText,
   Paper,
+  TextField,
 } from "@material-ui/core";
 import { useEffect, useState } from "react";
 import { get_users_with_detail } from "../utils/apis";
 import ImageIcon from "@material-ui/icons/Image";
 import AmountDecorate from "../utils/AmountDecorate";
+import { DeleteOutline, Refresh, Sort } from "@material-ui/icons";
+import {
+  amber,
+  blue,
+  blueGrey,
+  brown,
+  green,
+  grey,
+  indigo,
+  lime,
+  red,
+  teal,
+} from "@material-ui/core/colors";
 
 export default function UserListPanel(props) {
   const [users, setUsers] = useState([]);
+  const [filteredUsers, setFilteredUsers] = useState([]);
+  const [sort, setSort] = useState("none");
   const { onClick, showAsset = false, showBalance = false } = props;
 
   useEffect(() => {
     if (users.length === 0) {
       get_users_with_detail(
         { showAsset: showAsset, showBalance: showBalance },
-        (data) => setUsers(data)
+        (data) => {
+          setUsers(data);
+          setFilteredUsers(data);
+        }
       );
     }
   }, [users, showAsset, showBalance]);
 
   function userItem() {
-    return users.map((user, idx) => {
-      const subtitle = showAsset
-        ? "میزان دارایی: " + user.assets
-        : showBalance
-        ? "میزان حساب: " + user.balance
-        : "";
+    return filteredUsers.map((user, idx) => {
       return (
         <ListItem
-          key={idx}
+          key={user.id}
           button
           divider
           onClick={() => onClick({ target: { name: "user", value: user } })}
@@ -58,9 +73,53 @@ export default function UserListPanel(props) {
       );
     });
   }
+
+  const sortData = () => {
+    if (sort === "ASC") {
+      setSort("none");
+      filteredUsers.sort((a, b) => a.id - b.id);
+      setFilteredUsers(filteredUsers);
+    } else if (sort === "none") {
+      setSort("DESC");
+      filteredUsers.sort((a, b) =>
+        showAsset ? b.assets - a.assets : b.balance - a.balance
+      );
+      setFilteredUsers(filteredUsers);
+    } else if (sort === "DESC") {
+      setSort("ASC");
+      filteredUsers.sort((a, b) =>
+        showAsset ? a.assets - b.assets : a.balance - b.balance
+      );
+      setFilteredUsers(filteredUsers);
+    }
+  };
+
   return (
     <Box component={Paper}>
-      <List>{userItem()}</List>
+      <Box display="flex" borderBottom={1} bgcolor={indigo[100]} p={2}>
+        <Box display="inline-block" width="85%">
+          <TextField fullWidth placeholder="جستجو" />
+        </Box>
+        <Box display="inline-block" width="15%" alignItems="left">
+          <IconButton
+            style={{
+              display: "inline-block",
+              padding: "0px",
+              marginLeft: "1px",
+            }}
+            onClick={() => sortData()}
+          >
+            <Sort color={green[500]} />
+          </IconButton>
+          <IconButton
+            style={{ display: "inline-block", padding: "0px" }}
+            onClick={() => setFilteredUsers(users)}
+          >
+            <Refresh />
+          </IconButton>
+        </Box>
+      </Box>
+      <List key={sort}>{userItem()}</List>
     </Box>
   );
 }
